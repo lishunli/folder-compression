b2e568bef0f80d65e9b73f1835f1ba87999bb6df
lishunli.me@gmail.com <lishunli.me@gmail.com@cc2a6c82-b117-9997-931d-ce5d09ab1b9c>
2011-12-28 12:46:15 +0000

    DEV: add final name for avoid version roll.

--- folder-compression-app/src/main/bin/runApp1.bat
+++ folder-compression-app/src/main/bin/runApp1.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-SET version=0.1.2-SNAPSHOT
 
-java -classpath ../../../target/folder-compression-app-%version%.jar com.googlecode.usc.folder.compression.App1
+java -classpath ../../../target/folder-compression-app.jar com.googlecode.usc.folder.compression.App1
 
 
 @PAUSE
--- folder-compression-app/src/main/bin/runApp2.bat
+++ folder-compression-app/src/main/bin/runApp2.bat
@@ -1,10 +1,9 @@
 @ECHO OFF
-SET version=0.1.2-SNAPSHOT
 
 rem you know.
 SET folderPath=c:\temp\
 
-java -classpath ../../../target/folder-compression-app-%version%.jar com.googlecode.usc.folder.compression.App2 -folderPath %folderPath% -compressionType ZIP -excludedWords ".svn | target | target-eclipse | .classpath | .project | .settings" -help 
+java -classpath ../../../target/folder-compression-app.jar com.googlecode.usc.folder.compression.App2 -folderPath %folderPath% -compressionType ZIP -excludedWords ".svn | target | target-eclipse | .classpath | .project | .settings" -help 
 
 
 @PAUSE
